6a39e250f87a279aa1b8caf1ba16afc94a17e00a
zeyadShapan <zeyadshapan2004@gmail.com>
2021-02-25 06:58:09 +0200

    Added readme

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 module.exports = app => {
-    app.use('/auth', require('./auth'))
-    app.use('/users', require('./users'))
+    app.use('/api/auth', require('./auth'))
+    app.use('/api/users', require('./users'))
 }
